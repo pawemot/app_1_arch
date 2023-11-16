d78f683fa48294c3eca77d9b4af407d71b4d022f
Paweł <pawemot031@gmail.com>
2023-11-16 13:28:17 +0100

    fix

--- myproject.py
+++ myproject.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.route("/")
 
 def hello():
-	return "<h1> Pierwsza aplikacja w Pythonie (Flask)"
+	return "<h1> Pierwsza aplikacja w Pythonie (Flask)123</h1>"
 
 if __name__ == "__main__":
 
